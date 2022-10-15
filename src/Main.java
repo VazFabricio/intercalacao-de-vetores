eeb174089a3c610bd273cdfcc04763b1c246b0b3
Fabrício Vaz <fabricio.vaz2004@gmail.com>
2022-10-15 14:47:25 -0300

    Adicionado funcao de preenchimento de array

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,11 @@
+import java.util.Scanner;
 public class Main {
+    public static void registraArray(Scanner sc, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Digite o numero para o index <" + (i) + "> : ");
+            arr[i] = sc.nextInt();
+        }
+    }
     public static int[] misturaArray(int arr1[], int arr2[]) {
         int[] arr3 = new int[20];
         int j = 0;
@@ -26,9 +33,19 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        int[] arr1 = {3, 5, 4, 2, 2, 5, 3, 2, 5, 9};
-        int[] arr2 = {7, 15, 20, 0, 18, 4, 55, 23, 8, 6};
 
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr1 = new int[10];
+        int[] arr2 = new int[10];
+
+        System.out.println("-----Vetor 01-----");
+        registraArray(sc, arr1);
+
+        System.out.println("-----Vetor 02-----");
+        registraArray(sc, arr2);
+
+        System.out.println("-----O vetor intercalado e: -----");
         exibeArray(misturaArray(arr1, arr2));
 
     }
